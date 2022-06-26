74de49a526d83fda75cffc0c617b107a169b47c8
Mateusz Wilk <business.mateusz.wilk@gmail.com>
2022-06-26 18:15:40 +0200

    Create PaginationSlice

---
+++ src/PaginationSlice.ts
@@ -0,0 +1,10 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+export const paginationSlice = createSlice({
+  name: 'pagination',
+  initialState: {},
+  reducers: {},
+})
+
+export const {  } = paginationSlice.actions
+export default paginationSlice.reducer
