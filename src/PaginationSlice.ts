97d0e46f807f26e93cf475b614571d42785d4905
Mateusz Wilk <business.mateusz.wilk@gmail.com>
2022-06-26 18:18:15 +0200

    Add initialState

--- src/PaginationSlice.ts
+++ src/PaginationSlice.ts
@@ -2,7 +2,15 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 export const paginationSlice = createSlice({
   name: 'pagination',
-  initialState: {},
+  initialState: {
+    value: {
+      perPage: 5,
+      page: 1,
+      totalPages: 0,
+      products: [],
+      totalProducts: 0,
+    },
+  },
   reducers: {},
 })
 
