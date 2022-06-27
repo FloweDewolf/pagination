fee6183bc5b61516f5338c300408b9b9c6ad98bc
Mateusz Wilk <business.mateusz.wilk@gmail.com>
2022-06-27 10:21:47 +0200

    Implement filtering by id

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useAppDispatch, useAppSelector } from './hooks'
-import { initialize, setTotalPages, changePage } from './PaginationSlice'
+import {
+  initialize,
+  setTotalPages,
+  changePage,
+  onInputChange,
+} from './PaginationSlice'
 
 import { Container, Table, Row, StyledUl } from './App.styles'
+import { log } from 'util'
 
 const App: React.FC = () => {
   const dispatch = useAppDispatch()
-  const { perPage, page, totalPages, products, totalProducts } = useAppSelector(
-    (state) => state.pagination.value
-  )
+  const { perPage, page, totalPages, products, totalProducts, input } =
+    useAppSelector((state) => state.pagination.value)
+
+  const inputRef = useRef<HTMLInputElement>(null)
+  let isFiltered = !!inputRef.current?.value
 
   useEffect(() => {
     fetch('https://reqres.in/api/products')
@@ -23,10 +31,25 @@ const App: React.FC = () => {
     return products.slice(startIndex, endIndex)
   }
 
+  const displayedFilteredProducts = () => {
+    const index = products.findIndex(
+      (product) => product['id'] === Number(input)
+    )
+    return products.slice(index, index + 1)
+  }
+
   return (
     <Container>
       <div>
-        <input type="number" id="input" className="input" placeholder=" " />
+        <input
+          ref={inputRef}
+          value={input}
+          onChange={(e) => dispatch(onInputChange(e.target.value))}
+          type="number"
+          id="input"
+          className="input"
+          placeholder=" "
+        />
         <label htmlFor="input" className="label">
           SORT BY ID NUMBER
         </label>
@@ -37,13 +60,21 @@ const App: React.FC = () => {
           <p>name</p>
           <p>year</p>
         </div>
-        {displayedProducts().map((product) => (
-          <Row key={product['id']} backgroundColor={product['color']}>
-            <p>{product['id']}</p>
-            <p>{product['name']}</p>
-            <p>{product['year']}</p>
-          </Row>
-        ))}
+        {isFiltered
+          ? displayedFilteredProducts().map((product) => (
+              <Row key={product['id']} backgroundColor={product['color']}>
+                <p>{product['id']}</p>
+                <p>{product['name']}</p>
+                <p>{product['year']}</p>
+              </Row>
+            ))
+          : displayedProducts().map((product) => (
+              <Row key={product['id']} backgroundColor={product['color']}>
+                <p>{product['id']}</p>
+                <p>{product['name']}</p>
+                <p>{product['year']}</p>
+              </Row>
+            ))}
       </Table>
       <StyledUl>
         <li>
--- src/PaginationSlice.ts
+++ src/PaginationSlice.ts
@@ -1,4 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { stat } from 'fs'
 
 export const paginationSlice = createSlice({
   name: 'pagination',
@@ -8,11 +9,13 @@ export const paginationSlice = createSlice({
       page: 1,
       totalPages: 0,
       products: [],
+      savedProducts: [],
       totalProducts: 0,
+      input: '',
     },
   },
   reducers: {
-    initialize: (state, action: PayloadAction<[]>) => {
+    initialize: (state, action: PayloadAction<any>) => {
       state.value = {
         ...state.value,
         products: action.payload,
@@ -28,8 +31,7 @@ export const paginationSlice = createSlice({
     changePage: (state, action: PayloadAction<number>) => {
       if (action.payload === 1 && state.value.page === state.value.totalPages) {
         return
-      }
-      else if (action.payload === -1 && state.value.page === 1) {
+      } else if (action.payload === -1 && state.value.page === 1) {
         return
       }
 
@@ -39,8 +41,15 @@ export const paginationSlice = createSlice({
           action.payload === 1 ? state.value.page + 1 : state.value.page - 1,
       }
     },
+    onInputChange: (state, action: PayloadAction<string>) => {
+      state.value = {
+        ...state.value,
+        input: action.payload,
+      }
+    },
   },
 })
 
-export const { initialize, setTotalPages, changePage } = paginationSlice.actions
+export const { initialize, setTotalPages, changePage, onInputChange } =
+  paginationSlice.actions
 export default paginationSlice.reducer
